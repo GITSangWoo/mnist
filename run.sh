8b3def9d5431a75c976cef2ed3d619ce5fe8c30d
GITSangWoo <dogcenta@gmail.com>
2024-09-23 18:04:51 +0900

    fix run.sh env

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-echo LINE_API_KEY=$LINE_API_KEY >> /etc/environment;
+# echo LINE_API_KEY=$LINE_API_KEY >> /etc/environment;
+env >> /etc/environment; 
 service cron start;uvicorn main:app --host 0.0.0.0 --port 8080 --reload

45ea8de0cbb88259735a0ab2fe1c535646e8de0c
GITSangWoo <dogcenta@gmail.com>
2024-09-24 15:45:02 +0900

    pred suc, manifest add

--- src/mnist/worker.py
+++ src/mnist/worker.py
@@ -1,5 +1,18 @@
 import jigeum.seoul
 import os 
+import numpy as np
+from PIL import Image
+from keras.models import load_model
+
+
+def get_model_path():
+    # import os ...
+    # 이 함수 파일의 절대 경로를 받아온다
+    f= __file__
+    # 절대 경로를 이용해 model.pkl의 경로를 조합
+    dir_name=os.path.dirname(f)
+    file_path=os.path.join(dir_name, "mnist240924.keras")
+    return file_path
 
 def run():
     """image_processing 테이블을 읽어서 가장 오래된 요청 하나씩을 처리"""
@@ -13,7 +26,37 @@ def run():
         print(f"작업 요청 시간:{jigeum.seoul.now()}:predict_result=null인 값이 없습니다.")
         return True
     # STEP 2
-    # RANDOM 으로 0 ~ 9 중 하나 값을 prediction_result 컬럼에 업데이트
+    # 모델로 예측한 값을  prediction_result 컬럼에 업데이트
+    # 모델 로드
+    file_path=get_model_path()
+    print(file_path)
+    model = load_model(file_path) 
+    # 사용자 이미지 불러오기 및 전처리
+    def preprocess_image(image_path):
+        img = Image.open(image_path).convert('L')  # 흑백 이미지로 변환
+        img = img.resize((28, 28))  # 크기 조정
+
+        # 흑백 반전
+        img = 255 - np.array(img)  # 흑백 반전
+    
+        img = np.array(img)
+        img = img.reshape(1, 28, 28, 1)  # 모델 입력 형태에 맞게 변형
+        img = img / 255.0  # 정규화
+        return img
+
+    def predict_digit(image_path):
+        img = preprocess_image(image_path)
+        prediction = model.predict(img)
+        digit = np.argmax(prediction)
+        return digit
+    
+    # 사용자 이미지 경로
+    image_path = '/home/centa/code/mnist/note/train_img/my_picture1.png'
+
+    # 예측 실행
+    predicted_digit = predict_digit(image_path)
+    print("예측된 숫자:", predicted_digit)
+    
     # 동시에 prediction_model, prediction_time 도 업데이트
     import random
     rnum= random.randint(0,9) 
